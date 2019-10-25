07717717871b1b67604e1e7a0d30b3a7e10f581c
Kacper <kap3r3k@gmail.com>
2019-10-25 15:11:24 +0200

    Project Start
    xD

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int playerIndex = 1;
+    private Rigidbody playerBody;
+
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        state = State.Normal;
+    }
+
+    public State state;
+    public enum State
+    {
+        Normal,   
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                PlayerMovement(playerIndex);
+                PlayerRTrig(playerIndex);
+                PlayerBButton(playerIndex);
+                PlayerLTrig(playerIndex);
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void PlayerMovement(int index)
+    {
+        float horizontal = Input.GetAxis("Horizontal" + index);
+        float vertical = Input.GetAxis("Vertical" + index);
+    }
+
+    private void PlayerRTrig(int index)
+    {
+        if(Input.GetAxis("RTrig" + index) > 0.0f)
+        {
+            
+        }
+    }
+
+    private void PlayerLTrig(int index)
+    {
+        if (Input.GetAxis("LTrig" + index) > 0.0f)
+        {
+
+        }
+    }
+
+    private void PlayerBButton(int index)
+    {
+        if (Input.GetAxis("BButton" + index) > 0.0f)
+        {
+            
+        }
+    }
+}
